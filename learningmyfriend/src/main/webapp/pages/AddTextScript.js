76755e61507e29f51c2633ac7372ede455f56426
Maria shehata <maria.sheahata17@ncf.edu>
2020-02-29 20:00:40 -0500

    List UI

--- learningmyfriend/src/main/webapp/pages/AddTextScript.js
+++ learningmyfriend/src/main/webapp/pages/AddTextScript.js
@@ -6,3 +6,14 @@ function addText(name) {
                  "<div contenteditable='true'>This is " + name + "</div></div>";
     document.makeDraggable(document.getElementById(name));
 }
+
+// List (Listener to thr add list button) 
+document.getElementById('add').addEventListener("click", addList)
+
+function addList(){
+  var text = document.getElementById('idea').value;     
+  var node = document.createElement("li");    
+  var textNode = document.createTextNode(text);    
+  node.appendChild(textNode);     
+  document.getElementById('list').appendChild(node);
+};
