47aa8d3b0f2289677ea5ca376222fe0533b50ab4
Jan Fic <43736433+janfic@users.noreply.github.com>
2020-03-04 21:00:43 -0500

    Update dropdownmenu.js

--- learningmyfriend/src/main/webapp/pages/dropdownmenu.js
+++ learningmyfriend/src/main/webapp/pages/dropdownmenu.js
@@ -1,16 +1,19 @@
 function myFunction(id) {
-  var e = document.getElementById(id);
-  document.getElementById(id).classList.toggle("show");
-  console.log(document.getElementById(id));
+    var e = document.getElementById(id);
+    closeAll(e);
+    document.getElementById(id).classList.toggle("show");
+    console.log(document.getElementById(id));
 }
 
 // Close the dropdown menu if the user clicks outside of it
-function closeAll() {
+function closeAll(e) {
     var dropdowns = document.getElementsByClassName("show");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      openDropdown.classList.remove('show');
+        var openDropdown = dropdowns[i];
+        if (e !== openDropdown) {
+            openDropdown.classList.remove('show');
+        }
     }
-};
+}
 
