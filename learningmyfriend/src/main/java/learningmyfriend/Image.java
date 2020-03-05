797f6221fc0695a787269642ab98cfc91fd81dc8
Tyrell2000 <44913702+Tyrell2000@users.noreply.github.com>
2020-03-05 18:15:14 -0500

    Finished Sound and Image Backend

--- learningmyfriend/src/main/java/learningmyfriend/Image.java
+++ learningmyfriend/src/main/java/learningmyfriend/Image.java
@@ -9,40 +9,77 @@ package learningmyfriend;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.*;
+import java.util.Arrays;
+import java.util.stream.StreamSupport;
+import java.io.IOException;
 import static java.nio.file.StandardCopyOption.*;
 import javafx.beans.property.StringProperty;
 import javafx.beans.property.SimpleStringProperty;
 
+
 /**
- *
- * @author Tyrell Daniels
- */
+*
+* @author Tyrell Daniels
+*/
 public class Image extends Object{
-    
+
     //private String imagePath, targetImagePath;
     private Path here, there;
-    
+    //StringProperty filePath = new SimpleStringProperty(this, "test", "");
+    private String fileName;
+
     public Image(){
-        //this.imagePath = "C:\\Users\\HNS1Lab.NETWORK\\Pictures\\OculusScreenshot1572893015.jpeg";
-        //this.targetImagePath = "\\Images";
-        
-        there = Paths.get("C:\\Users\\HNS1Lab.NETWORK\\Pictures\\OculusScreenshot1572893015.jpeg");
-        here = Paths.get("\\Images");
-        
-        try{
-            Files.copy(this.there, this.here, REPLACE_EXISTING);
-        }
-        catch(Exception e){}
+
+        //this.there = Paths.get("C:\\Users\\HNS1Lab.NETWORK\\Pictures\\OculusScreenshot1572893034.jpeg");
+
+        //String[] paths = splitPath(this.there.toString());
+        //Arrays.toString(paths);
+
+        //for (String name: paths){
+            //this.fileName = name;
+        //}
+
+        //this.here = Paths.get("C:\\Users\\HNS1Lab.NETWORK\\Downloads\\Images\\" + this.fileName);
     }
 
     public Path getImagePath() {
-        return there;
+        return this.there;
     }
 
-    public void setImagePath(Path ImagePath) {
-        this.there = ImagePath;
+    public Path getNewImagePath() {
+        return this.here;
     }
 
-   
+    public void setImagePath(String ImagePath) {
+        this.there = Paths.get(ImagePath);
+
+        String[] paths = splitPath(this.there.toString());
+        Arrays.toString(paths);
+
+        for (String name: paths){
+            this.fileName = name;
+        }
+
+        this.here = Paths.get("C:\\Users\\HNS1Lab.NETWORK\\Downloads\\Images\\" + this.fileName);
+    }
+
+    public void copyImage(){
+        try{
+            Files.copy(this.there, this.here, REPLACE_EXISTING);
+        }
+        catch(Exception e){
+        }
+    }
     
+    /**
+     *
+     * @author https://www.logicbig.com/how-to/java-io/split-path.html
+     */
+    public static String[] splitPath(String pathString) {
+        Path path = Paths.get(pathString);
+        return StreamSupport.stream(path.spliterator(), false).map(Path::toString)
+                .toArray(String[]::new);
+    }
+
 }
--- learningmyfriend/src/main/java/learningmyfriend/Sound.java
+++ learningmyfriend/src/main/java/learningmyfriend/Sound.java
@@ -9,40 +9,77 @@ package learningmyfriend;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.*;
+import java.util.Arrays;
+import java.util.stream.StreamSupport;
+import java.io.IOException;
 import static java.nio.file.StandardCopyOption.*;
 import javafx.beans.property.StringProperty;
 import javafx.beans.property.SimpleStringProperty;
+
+
 
 /**
  *
  * @author Tyrell Daniels
  */
 public class Sound extends Object{
-    
+
     //private String imagePath, targetImagePath;
-    private Path here, there, here2, there2;
-    
+    private Path here, there;
+    //StringProperty filePath = new SimpleStringProperty(this, "test", "");
+    private String fileName;
+
     public Sound(){
-        //this.imagePath = "C:\\Users\\HNS1Lab.NETWORK\\Pictures\\OculusScreenshot1572893015.jpeg";
-        //this.targetImagePath = "\\Images";
-        
-        there = Paths.get("C:\\Users\\HNS1Lab.NETWORK\\Videos\\JuiceWRLD.mp3");
-        here = Paths.get("\\Sounds");
-        
+
+        //this.there = Paths.get("C:\\Users\\HNS1Lab.NETWORK\\Videos\\JuiceWRLD.mp3");
+
+        //String[] paths = splitPath(this.there.toString());
+        //Arrays.toString(paths);
+
+        //for (String name: paths){
+            //this.fileName = name;
+        //}
+
+        //this.here = Paths.get("C:\\Users\\HNS1Lab.NETWORK\\Downloads\\Sounds\\" + this.fileName);
+    }
+
+    public Path getSoundPath() {
+        return this.there;
+    }
+
+    public Path getNewSoundPath() {
+        return this.here;
+    }
+
+    public void setSoundPath(String SoundPath) {
+        this.there = Paths.get(SoundPath);
+
+        String[] paths = splitPath(this.there.toString());
+        Arrays.toString(paths);
+
+        for (String name: paths){
+            this.fileName = name;
+        }
+
+        this.here = Paths.get("C:\\Users\\HNS1Lab.NETWORK\\Downloads\\test\\" + this.fileName);
+    }
+
+    public void copySound(){
         try{
             Files.copy(this.there, this.here, REPLACE_EXISTING);
         }
         catch(Exception e){}
     }
-
-    public Path getSoundPath() {
-        return there;
-    }
-
-    public void setSoundPath(Path SoundPath) {
-        this.there = SoundPath;
-    }
-
-   
     
+    /**
+     *
+     * @author https://www.logicbig.com/how-to/java-io/split-path.html
+     */
+    public static String[] splitPath(String pathString) {
+        Path path = Paths.get(pathString);
+        return StreamSupport.stream(path.spliterator(), false).map(Path::toString)
+                .toArray(String[]::new);
+    }
+
 }
