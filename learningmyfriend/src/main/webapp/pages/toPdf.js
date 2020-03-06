da723a4a751b9824cf5974b2a510845ca7563ab0
Jan Fic <43736433+janfic@users.noreply.github.com>
2020-03-05 23:32:34 -0500

    added print and pdf

--- learningmyfriend/src/main/webapp/pages/toPdf.js
+++ learningmyfriend/src/main/webapp/pages/toPdf.js
@@ -1,16 +1,14 @@
-function togglePresent() {
+function toPDF() {
     var slide = document.getElementById("slideedit");
     var slideContents = slide.innerHTML;
 
     var params = [
-        'height=' + 800,
-        'width=' + 1100,
-        'fullscreen=yes' // only works in IE, but here for completeness
+        'height=' + 850,
+        'width=' + 1100
     ].join(',');
 
     var printWindow = window.open('', '', params);
-    printWindow.document.write('<html><head><title>PDF SAVE</title>');
-
+    printWindow.document.write('<html><head><title>DIV Contents</title>');
     printWindow.document.write('<link rel="stylesheet" type="text/css" href="index.css" />');
     printWindow.document.write('</head><body >');
     printWindow.document.write('<div id="slideedit">');
@@ -18,4 +16,6 @@ function togglePresent() {
     printWindow.document.write('</div>');
     printWindow.document.write('</body></html>');
     printWindow.document.close();
+    printWindow.setTimeout(function(){printWindow.print(); printWindow.close();;}, 2000);
+    
 }
